9cfd9c4bedab583dc4804e62b39b8b0d76d46f36
Mohd Zain <zainmohd@sigmoidanalytics.com>
2025-05-08 15:54:31 +0530

    add smaller dataset

--- core/data_loader.py
+++ core/data_loader.py
@@ -5,5 +5,5 @@ class Data:
     def __init__(self):
         print("loading data in data_loader")
         self.data_description = pd.read_csv("./data/origination_feats.csv")
-        self.data = pd.read_excel("./data/morigination.xlsx", sheet_name="Sheet1")
+        self.data = pd.read_excel("./data/small_data.xlsx", sheet_name="Sheet1")
         print("data loaded in data loader")
